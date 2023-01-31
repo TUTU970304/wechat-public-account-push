0d46760f8b777572ea263e3dbf6aaf1842772a32
TUTU970304 <71417540+TUTU970304@users.noreply.github.com>
2023-01-31 15:17:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -15,46 +15,46 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'ef854ccc8356db9c974456567e1d905a',
   // 功能开关,打开：true，关闭：false
-    SWITCH: {
-      /** 每日天气 */
-      // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-      weather: true,
+  SWITCH: {
+    /** 每日天气 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    weather: true,
 
-      /** 节假日 */
-      // 下一休息日综合提醒, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-      holidaytts: true,
+    /** 节假日 */
+    // 下一休息日综合提醒, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    holidaytts: true,
 
-      /** 每日N句 */
-      // 金山每日一句, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-      CIBA: true,
+    /** 每日N句 */
+    // 金山每日一句, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    CIBA: true,
 
-      // 每日一言, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-      oneTalk: true,
+    // 每日一言, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    oneTalk: true,
 
-      // 土味情话(彩虹屁), 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-      earthyLoveWords: true,
+    // 土味情话(彩虹屁), 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    earthyLoveWords: true,
 
-      // 朋友圈文案, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-      momentCopyrighting: false,
+    // 朋友圈文案, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    momentCopyrighting: false,
 
-      // 毒鸡汤, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-      poisonChickenSoup: false,
+    // 毒鸡汤, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    poisonChickenSoup: false,
 
-      // 古诗古文, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-      poetry: false,
+    // 古诗古文, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    poetry: false,
 
-      /** 星座运势 */
-      // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-      horoscope: true,
+    /** 星座运势 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    horoscope: true,
 
-      /** 生日消息和节日消息 */
-      // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-      birthdayMessage: false,
+    /** 生日消息和节日消息 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    birthdayMessage: false,
 
-      /** 学生课表 */
-      // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-      courseSchedule: false,
-    },
+    /** 学生课表 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    courseSchedule: false,
+  },
   
   /** 是否给文字设置多彩颜色, 和emoji不兼容 */
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
