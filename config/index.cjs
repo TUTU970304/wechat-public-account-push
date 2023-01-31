744967ea6ce067fd232b493dbce9ed5bcff4d31c
TUTU970304 <71417540+TUTU970304@users.noreply.github.com>
2023-01-31 09:48:45 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -64,18 +64,6 @@ const USER_CONFIG = {
     },
   ],
 
-
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'qIOsXBXXr7zQt6I2fvg8Ehbz-m4Wzvk1Hq9I37DvwEI',
-
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o--sv5sD9z6vHyniVseZokp9WAck',
-    }
-  ],
-
 }
 
 module.exports = USER_CONFIG
