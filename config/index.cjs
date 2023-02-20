fbea9e8e779e4d1439e3d03b7efa15796dbf31fb
TUTU970304 <71417540+TUTU970304@users.noreply.github.com>
2023-02-20 10:51:37 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -64,14 +64,197 @@ const USER_CONFIG = {
   CITY: '宁波',
 
   USERS: [
-    {
+//     {
+//       name: '史蕾小宝宝',
+//       id: 'o--sv5vFcGL8PECQkR_IrsxMvunQ',
+//       useTemplateId: 'D84uaHMgrvrhNe37ChKviifFDWc-kuqmA7xdzJZgeZQ',
+//       // 生日
+//       festivals:[
+//         {"type":"生日","name":"小笨蛋","date":"07-16","year":"1993"},
+//         {"type":"生日","name":"大宝贝","date":"03-04","year":"1997"},
+//         {"type":"节日","name":"拍婚纱","date":"06-15","year":"2023"},
+//         {"type":"节日","name":"结婚","date":"10-03","year":"2023"}
+//         // {"type":"生日","name":"birthday2_message","date":"03-04","year":"1997"}
+//       ],
+//       // 我们在一起已经有xxxx天了的配置
+//       customizedDateList: [
+//         // 相见的日子
+//         { keyword: 'meet_day', date: '2022-07-16' },
+//         // 在一起的日子
+//         { keyword: 'love_day', date: '2022-09-15' }
+//         // 结婚纪念日
+//         // { keyword: 'marry_day', date: '2022-09-09' }
+//       ],   
+//       holidayttsDateList: [
+//         // 节假日
+//         { keyword: 'holidaytts' }
+//       ],
+//       /** 每日一言 */
+//       // 每日一言的内容类型
+//       // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
+//       LITERARY_PREFERENCE: '',
+//       dayDateList: [
+//         // 每日一句
+//         // { keyword: 'note_ch' },
+//         // 每日一言
+//         { keyword: 'one_talk' },
+//         // 土味情话(彩虹屁)
+//         { keyword: 'earthy_love_words' }
+//       ],
+//     },
+//     {
+//       // 想要发送的人的名字
+//       name: '史蕾小宝宝',
+//       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+//       id: 'o--sv5vFcGL8PECQkR_IrsxMvunQ',
+//       // 使用微信测试号：你想对他发送的模板消息的模板ID
+//       useTemplateId: 'MoUEv_JgYag7eeJLEGg2npLvSASpmjNWMU7hp3H4mbQ',
+
+//       weatherDateList: [
+//         // 天气
+//         { keyword: 'weather' },
+//         // 最低气温
+//         { keyword: 'min_temperature' },
+//         // 最高气温
+//         { keyword: 'max_temperature' },
+//         // 风向
+//         { keyword: 'wind_direction' },
+//         // 风级
+//         { keyword: 'wind_scale'},
+//         // 湿度
+//         { keyword: 'shidu'},
+//         // PM2.5
+//         { keyword: 'pm25'},
+//         // 日出时间
+//         { keyword: 'sunrise'},
+//         // 日落时间
+//         { keyword: 'sunset'},
+//         // 空气质量指数
+//         { keyword: 'aqi'},
+//         // 预防感冒提醒
+//         { keyword: 'ganmao'},
+//         // 天气温馨语
+//         { keyword: 'notice'}
+//       ],
+//     },
+//     {
+//       id: 'o--sv5vFcGL8PECQkR_IrsxMvunQ',
+//       useTemplateId: 'qb-BwnRoWCB-T8z4B4x5zT9abgx_m2j-cVLvahVoELw',
+//       // 新历生日, 仅用作获取星座运势, 格式必须
+//       horoscopeDate: '07-16',
+//       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+//       horoscopeDateType: '今日',
+//       literaryDateList: [
+//         // 综合运势
+//         { keyword: 'comprehensive_horoscope' },
+//         // 事业学业
+//         { keyword: 'career_horoscope' },
+//         // 财富运势
+//         { keyword: 'wealth_horoscope' }
+//       ],
+//     },
+//      {
+//       id: 'o--sv5vFcGL8PECQkR_IrsxMvunQ',
+//       useTemplateId: 'GxuhEKXZ3-Iu-u25fgbWGD9ubI38afsHXyjyTxfjduw',
+//       // 新历生日, 仅用作获取星座运势, 格式必须
+//       horoscopeDate: '07-16',
+//       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+//       horoscopeDateType: '今日',
+//       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+//       openUrl: 'https://weibo.com/',
+//       literaryDateList: [
+//         // 健康运势
+//         { keyword: 'healthy_horoscope' }
+//       ],
+//     },
+//     {
+//       name: '小蒋',
+//       id: 'o--sv5jfPIk4J0XO5DQKnlY2xolM',
+//       useTemplateId: 'gp1UtPlbZhaujm0jJ-q36Cs8kkyWX-gNibLrtrgMqGU',
+//       // 生日
+//       festivals:[
+//         {"type":"生日","name":"小蒋","date":"09-14","year":"1996"},
+//         {"type":"生日","name":"HC","date":"03-25","year":"1997"}       
+//         // {"type":"生日","name":"birthday2_message","date":"03-04","year":"1997"}
+//       ],
+//       // 我们在一起已经有xxxx天了的配置
+//       customizedDateList: [
+//         // 相见的日子
+//         //{ keyword: 'meet_day', date: '2022-07-16' },
+//         // 在一起的日子
+//         { keyword: 'love_day', date: '2020-03-29' }
+//         // 结婚纪念日
+//         // { keyword: 'marry_day', date: '2022-09-09' }
+//       ],   
+//       holidayttsDateList: [
+//         // 节假日
+//         { keyword: 'holidaytts' }
+//       ],
+//       /** 每日一言 */
+//       // 每日一言的内容类型
+//       // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
+//       LITERARY_PREFERENCE: '',
+//       dayDateList: [
+//         // 每日一句
+//         // { keyword: 'note_ch' },
+//         // 每日一言
+//         { keyword: 'one_talk' },
+//         // 土味情话(彩虹屁)
+//         { keyword: 'earthy_love_words' }
+//       ],
+//       weatherDateList: [
+//         // 天气
+//         { keyword: 'weather' },
+//         // 最低气温
+//         { keyword: 'min_temperature' },
+//         // 最高气温
+//         { keyword: 'max_temperature' },
+//         // 风向
+//         { keyword: 'wind_direction' },
+//         // 风级
+//         { keyword: 'wind_scale'},
+//         // 湿度
+//         { keyword: 'shidu'},
+//         // PM2.5
+//         { keyword: 'pm25'},
+//         // 日出时间
+//         { keyword: 'sunrise'},
+//         // 日落时间
+//         { keyword: 'sunset'},
+//         // 空气质量指数
+//         { keyword: 'aqi'},
+//         // 预防感冒提醒
+//         { keyword: 'ganmao'},
+//         // 天气温馨语
+//         { keyword: 'notice'}
+//       ],
+//     },
+//      {
+//       id: 'o--sv5jfPIk4J0XO5DQKnlY2xolM',
+//       useTemplateId: 'hsPwMjRbP9_D1gVHlFS31vpXC9L4Kjp5exfN2_ccrDg',
+//       // 新历生日, 仅用作获取星座运势, 格式必须
+//       horoscopeDate: '09-14',
+//       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+//       horoscopeDateType: '今日',
+//       literaryDateList: [
+//         // 综合运势
+//         { keyword: 'comprehensive_horoscope' },
+//         // 事业学业
+//         // { keyword: 'career_horoscope' },
+//         // 财富运势
+//         // { keyword: 'wealth_horoscope' }
+//       ],
+//     },
+        {
       name: '史蕾小宝宝',
-      id: 'o--sv5vFcGL8PECQkR_IrsxMvunQ',
+      id: 'o--sv5sD9z6vHyniVseZokp9WAck',
       useTemplateId: 'D84uaHMgrvrhNe37ChKviifFDWc-kuqmA7xdzJZgeZQ',
       // 生日
       festivals:[
         {"type":"生日","name":"小笨蛋","date":"07-16","year":"1993"},
-        {"type":"生日","name":"大宝贝","date":"03-04","year":"1997"}       
+        {"type":"生日","name":"大宝贝","date":"03-04","year":"1997"},
+        {"type":"节日","name":"拍婚纱","date":"06-15","year":"2023"},
+        {"type":"节日","name":"结婚","date":"10-03","year":"2023"}
         // {"type":"生日","name":"birthday2_message","date":"03-04","year":"1997"}
       ],
       // 我们在一起已经有xxxx天了的配置
@@ -98,149 +281,6 @@ const USER_CONFIG = {
         { keyword: 'one_talk' },
         // 土味情话(彩虹屁)
         { keyword: 'earthy_love_words' }
-      ],
-    },
-    {
-      // 想要发送的人的名字
-      name: '史蕾小宝宝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o--sv5vFcGL8PECQkR_IrsxMvunQ',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'MoUEv_JgYag7eeJLEGg2npLvSASpmjNWMU7hp3H4mbQ',
-
-      weatherDateList: [
-        // 天气
-        { keyword: 'weather' },
-        // 最低气温
-        { keyword: 'min_temperature' },
-        // 最高气温
-        { keyword: 'max_temperature' },
-        // 风向
-        { keyword: 'wind_direction' },
-        // 风级
-        { keyword: 'wind_scale'},
-        // 湿度
-        { keyword: 'shidu'},
-        // PM2.5
-        { keyword: 'pm25'},
-        // 日出时间
-        { keyword: 'sunrise'},
-        // 日落时间
-        { keyword: 'sunset'},
-        // 空气质量指数
-        { keyword: 'aqi'},
-        // 预防感冒提醒
-        { keyword: 'ganmao'},
-        // 天气温馨语
-        { keyword: 'notice'}
-      ],
-    },
-    {
-      id: 'o--sv5vFcGL8PECQkR_IrsxMvunQ',
-      useTemplateId: 'qb-BwnRoWCB-T8z4B4x5zT9abgx_m2j-cVLvahVoELw',
-      // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '07-16',
-      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
-      horoscopeDateType: '今日',
-      literaryDateList: [
-        // 综合运势
-        { keyword: 'comprehensive_horoscope' },
-        // 事业学业
-        { keyword: 'career_horoscope' },
-        // 财富运势
-        { keyword: 'wealth_horoscope' }
-      ],
-    },
-     {
-      id: 'o--sv5vFcGL8PECQkR_IrsxMvunQ',
-      useTemplateId: 'GxuhEKXZ3-Iu-u25fgbWGD9ubI38afsHXyjyTxfjduw',
-      // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '07-16',
-      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
-      horoscopeDateType: '今日',
-      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: 'https://weibo.com/',
-      literaryDateList: [
-        // 健康运势
-        { keyword: 'healthy_horoscope' }
-      ],
-    },
-    {
-      name: '小蒋',
-      id: 'o--sv5jfPIk4J0XO5DQKnlY2xolM',
-      useTemplateId: 'gp1UtPlbZhaujm0jJ-q36Cs8kkyWX-gNibLrtrgMqGU',
-      // 生日
-      festivals:[
-        {"type":"生日","name":"小蒋","date":"09-14","year":"1996"},
-        {"type":"生日","name":"HC","date":"03-25","year":"1997"}       
-        // {"type":"生日","name":"birthday2_message","date":"03-04","year":"1997"}
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 相见的日子
-        //{ keyword: 'meet_day', date: '2022-07-16' },
-        // 在一起的日子
-        { keyword: 'love_day', date: '2020-03-29' }
-        // 结婚纪念日
-        // { keyword: 'marry_day', date: '2022-09-09' }
-      ],   
-      holidayttsDateList: [
-        // 节假日
-        { keyword: 'holidaytts' }
-      ],
-      /** 每日一言 */
-      // 每日一言的内容类型
-      // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
-      LITERARY_PREFERENCE: '',
-      dayDateList: [
-        // 每日一句
-        // { keyword: 'note_ch' },
-        // 每日一言
-        { keyword: 'one_talk' },
-        // 土味情话(彩虹屁)
-        { keyword: 'earthy_love_words' }
-      ],
-      weatherDateList: [
-        // 天气
-        { keyword: 'weather' },
-        // 最低气温
-        { keyword: 'min_temperature' },
-        // 最高气温
-        { keyword: 'max_temperature' },
-        // 风向
-        { keyword: 'wind_direction' },
-        // 风级
-        { keyword: 'wind_scale'},
-        // 湿度
-        { keyword: 'shidu'},
-        // PM2.5
-        { keyword: 'pm25'},
-        // 日出时间
-        { keyword: 'sunrise'},
-        // 日落时间
-        { keyword: 'sunset'},
-        // 空气质量指数
-        { keyword: 'aqi'},
-        // 预防感冒提醒
-        { keyword: 'ganmao'},
-        // 天气温馨语
-        { keyword: 'notice'}
-      ],
-    },
-     {
-      id: 'o--sv5jfPIk4J0XO5DQKnlY2xolM',
-      useTemplateId: 'hsPwMjRbP9_D1gVHlFS31vpXC9L4Kjp5exfN2_ccrDg',
-      // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '09-14',
-      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
-      horoscopeDateType: '今日',
-      literaryDateList: [
-        // 综合运势
-        { keyword: 'comprehensive_horoscope' },
-        // 事业学业
-        // { keyword: 'career_horoscope' },
-        // 财富运势
-        // { keyword: 'wealth_horoscope' }
       ],
     },
   ],
